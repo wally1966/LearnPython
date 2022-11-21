bae671dbd6b414b82de65586453d0432d07ea40a
wally1966 <walterrieppi@gmail.com>
2022-11-21 22:01:06 +0100

    Esercizio 12 e 13

---
+++ ex12.py
@@ -0,0 +1,6 @@
+nome = input("Come ti chiami ? \t")
+eta = input("Quanti anni hai ? \t")
+altezza = input("Quanto sei alto ? \t")
+print (f"Sei {nome} e hai {eta} anni e sei alto {altezza} cm")
+
+
---
+++ ex13.py
@@ -0,0 +1,7 @@
+from sys import argv
+
+nome = argv[1]
+eta = argv[2]
+altezza = argv[3]
+print (f"Sei {nome} e hai {eta} anni e sei alto {altezza} cm")
+
