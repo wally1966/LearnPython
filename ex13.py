fdf38908156e0b2f2f0b3b634ac7abe222391261
wally1966 <walterrieppi@gmail.com>
2022-11-23 20:48:38 +0100

    Esercizio 13

--- ex13.py
+++ ex13.py
@@ -3,5 +3,5 @@ nome = argv[1]
 nome = argv[1]
 eta = argv[2]
 altezza = argv[3]
-print (f"Sei {nome} e hai {eta} anni e sei alto {altezza} cm")
+print (f"Sei {nome} e hai {eta} anni e sei    alto {altezza} cm")
 
